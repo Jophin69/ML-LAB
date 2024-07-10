46e0118aa6c41dd64bd6f94c42a614c5bc067c81
Jophin Benet <jophinbenet.k.34@gmail.com>
2024-07-10 09:28:32 +0530

    LP8 UPDATED

--- lp8.py
+++ lp8.py
@@ -1,3 +1,4 @@
+#NAIVE BAYES FOR SPECIFIC DATASET
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,classification_report
